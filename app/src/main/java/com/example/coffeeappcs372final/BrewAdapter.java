33f6153378a413bb224d571aa28a01487ae2a3ee
Cy Dixon <94246986+cylissama@users.noreply.github.com>
2024-04-20 17:38:50 -0500

    added dialog boxes for brew_items

--- app/src/main/java/com/example/coffeeappcs372final/BrewAdapter.java
+++ app/src/main/java/com/example/coffeeappcs372final/BrewAdapter.java
@@ -1,15 +1,25 @@
 package com.example.coffeeappcs372final;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+import java.util.Objects;
 
 public class BrewAdapter extends RecyclerView.Adapter<BrewAdapter.ViewHolder> {
 
@@ -37,6 +47,45 @@ public class BrewAdapter extends RecyclerView.Adapter<BrewAdapter.ViewHolder> {
                 "\nBrewer: " + brew.getBrewer();
 
         holder.brewView.setText(text);
+
+        holder.brewContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+                // Example: Log or Toast
+                //Toast.makeText(v.getContext(), "Button clicked at position " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void showDialog() {
+
+        final Dialog dialog = new Dialog(inflater.getContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottom_sheet_layout);
+
+        LinearLayout editLayout = dialog.findViewById(R.id.layoutEdit);
+        LinearLayout editLayout2 = dialog.findViewById(R.id.layoutEdit2);
+
+        editLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        editLayout2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        dialog.show();
+        Objects.requireNonNull(dialog.getWindow()).setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+
     }
 
     @Override
@@ -46,10 +95,12 @@ public class BrewAdapter extends RecyclerView.Adapter<BrewAdapter.ViewHolder> {
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView brewView;
+        CardView brewContainer;
 
         ViewHolder(View itemView) {
             super(itemView);
             brewView = itemView.findViewById(R.id.brewView);
+            brewContainer = itemView.findViewById(R.id.brewContainer);
         }
     }
 }
